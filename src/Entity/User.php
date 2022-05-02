3964d8e82d10a3bf527503c48fdc31ebe712f7d7
benjamin plesant <benjami.plesant@outlook.fr>
2022-05-02 22:37:11 +0200

    - prePersist / HasLifecycleCallbacks - prePersist role, token ok

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,13 +4,15 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\HasLifecycleCallbacks;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
-#[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Entity(repositoryClass: UserRepository::class), HasLifecycleCallbacks]
 #[ORM\Table(name: '`user`')]
 #[UniqueEntity(fields: ['username'], message: 'There is already an account with this username')]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -30,6 +32,15 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(type: 'string', length: 255, unique: true)]
     private $email;
 
+    #[ORM\Column(type: 'string', length: 255)]
+    private $accessToken;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private $refreshToken;
+
+    #[ORM\Column(type: 'datetime_immutable', nullable: true)]
+    private $refreshTokenAt;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -47,6 +58,18 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
+    #[ORM\PrePersist]
+    public function prePersist()
+    {
+        if (!empty($this->getRoles())) {
+            $this->setRoles(['ROLE_USER']);
+        }
+
+        if (empty($this->getAccessToken())) {
+            $this->setAccessToken(rtrim(strtr(base64_encode(random_bytes(32)), '+/', '-_'), '='));
+        }
+    }
+
     /**
      * A visual identifier that represents this user.
      *
@@ -54,7 +77,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function getUserIdentifier(): string
     {
-        return (string) $this->username;
+        return (string)$this->username;
     }
 
     /**
@@ -108,6 +131,42 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setEmail(string $email): self
     {
         $this->email = $email;
+
+        return $this;
+    }
+
+    public function getAccessToken(): ?string
+    {
+        return $this->accessToken;
+    }
+
+    public function setAccessToken(string $accessToken): self
+    {
+        $this->accessToken = $accessToken;
+
+        return $this;
+    }
+
+    public function getRefreshToken(): ?string
+    {
+        return $this->refreshToken;
+    }
+
+    public function setRefreshToken(?string $refreshToken): self
+    {
+        $this->refreshToken = $refreshToken;
+
+        return $this;
+    }
+
+    public function getRefreshTokenAt(): ?\DateTimeImmutable
+    {
+        return $this->refreshTokenAt;
+    }
+
+    public function setRefreshTokenAt(?\DateTimeImmutable $refreshTokenAt): self
+    {
+        $this->refreshTokenAt = $refreshTokenAt;
 
         return $this;
     }
