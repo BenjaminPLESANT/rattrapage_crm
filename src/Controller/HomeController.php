1580f2dba5af25bb3d6d435e2656baecad27449b
benjamin plesant <benjami.plesant@outlook.fr>
2022-05-08 19:58:32 +0200

    - SERIALIZER OBJECT TO ARRAY !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,72 +2,32 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
 use App\Repository\UserRepository;
-use App\Service\SerializerService;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Serializer\SerializerInterface;
+
 
 class HomeController extends AbstractController
 {
-    private SerializerService $serializer;
-    private Security $security;
 
 
-    public function __construct(SerializerService $serializer, Security $security)
-    {
-        $this->serializer = $serializer;
-        $this->security = $security;
-    }
 
 
     #[IsGranted('ROLE_USER', message: "Vous n'avez pas le droit d'être ici", statusCode: 403)]
     #[Route('/account', name: 'account')]
-    public function account(UserRepository $userRepository, Request $request,): Response
+    public function account(UserRepository $userRepository, Request $request, SerializerInterface $serializer): Response
     {
+        $user = $this->getUser();
+        $entityAsArray = $serializer->normalize($user, 'array');
 
-        $user = $this->security->getUser();
-        $userId = $user-
+        dump($entityAsArray);
         dump($user);
 
-//        $content = $this->getUser();
-//        $serializedContent = $this->serializer->SimpleSerializer($content, 'json');
-//        if (array_key_exists('id', $content)) {
-//            $id =
-//        }
-//        $array = object_to_array($serializedContent);
-//
-//        dump($content);
-//        dump($array);
 
-
-//        $request->getSession()->set(Security::LAST_USERNAME, $username);
-
-
-//        $content = $response->toArray();
-
-
-////
-//        $user = $this->getUser();
-//        $userRole = $user->getRoles();
-//
-//        if ($user && $userRole = 'ROLE_USER') {
-//            $this->event->setResponse(new RedirectResponse($this->redirectToRoute('app_access_denied_handler')));
-//        } else {
-//            dump('c de la merde putain de sa mere fdp');
-//        }
-
-
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-
-        ]);
+        return $this->render('home/index.html.twig', ['controller_name' => 'HomeController',]);
     }
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -13,7 +13,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 #[ORM\Table(name: '`user`')]
 #[UniqueEntity(fields: ['username'], message: 'There is already an account with this username')]
 #[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
-abstract class User implements UserInterface, PasswordAuthenticatedUserInterface
+class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
@@ -204,5 +204,8 @@ abstract class User implements UserInterface, PasswordAuthenticatedUserInterface
     }
 
 
-
+    public function getSalt()
+    {
+        // TODO: Implement getSalt() method.
+    }
 }
