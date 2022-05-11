e78e4b7f97b418d4f4d446f00bc59dd6c7d60076
benjamin plesant <benjami.plesant@outlook.fr>
2022-05-11 23:07:01 +0200

    - Login Ok - Middleware connecté OK

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,21 +3,30 @@
 namespace App\Controller;
 
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Serializer\SerializerInterface;
 
 
 class HomeController extends AbstractController
 {
+    private UserRepository $user;
+    private EntityManagerInterface $em;
 
+    public function __construct(EntityManagerInterface $em, UserRepository $user)
+    {
+        $this->em = $em;
+        $this->user = $user;
+    }
 
     #[IsGranted('ROLE_USER', message: "Vous n'avez pas le droit d'être ici", statusCode: 403)]
     #[Route('/account', name: 'account')]
-    public function account(UserRepository $userRepository, Request $request, SerializerInterface $serializer): Response
+    public function account(Request $request, SerializerInterface $serializer): Response
     {
         $user = $this->getUser();
         $data = $serializer->normalize($user, 'array');
@@ -31,8 +40,7 @@ class HomeController extends AbstractController
 
         }
 
-        $putain = $request->getSession();
-        dump($putain);
+
 
         return $this->render('home/index.html.twig', ['controller_name' => 'HomeController',]);
     }
